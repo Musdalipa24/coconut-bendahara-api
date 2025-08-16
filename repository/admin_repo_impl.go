05f6f7015893d1c811473a6ee41df37cf7cb204b
syrlramadhan <syahrul0422@gmail.com>
2025-08-16 20:13:31 +0800

    .

--- repository/admin_repo_impl.go
+++ repository/admin_repo_impl.go
@@ -23,7 +23,7 @@ func NewAdminRepo() AdminRepo {
 
 // SignUp implements AdminRepo.
 func (a adminRepoImpl) SignUp(ctx context.Context, tx *sql.Tx, admin model.Admin) (model.Admin, error) {
-	query := "INSERT INTO Admin (id, username, password) VALUES (?, ?, ?)"
+	query := "INSERT INTO admin (id, username, password) VALUES (?, ?, ?)"
 
 	_, err := tx.ExecContext(ctx, query, admin.Id, admin.Username, admin.Password)
 	if err != nil {
@@ -35,7 +35,7 @@ func (a adminRepoImpl) SignUp(ctx context.Context, tx *sql.Tx, admin model.Admin
 
 // FindById implements AdminRepo.
 func (a adminRepoImpl) FindByUsername(ctx context.Context, tx *sql.Tx, username string) (model.Admin, error) {
-	query := "SELECT id, username, password FROM Admin WHERE username = ?"
+	query := "SELECT id, username, password FROM admin WHERE username = ?"
 
 	rows, err := tx.QueryContext(ctx, query, username)
 	if err != nil {
@@ -57,7 +57,7 @@ func (a adminRepoImpl) FindByUsername(ctx context.Context, tx *sql.Tx, username 
 
 // UpdateAdmin implements AdminRepo.
 func (a *adminRepoImpl) UpdateAdmin(ctx context.Context, tx *sql.Tx, admin model.Admin) (model.Admin, error) {
-	query := "UPDATE Admin SET password = ? WHERE username = ?"
+	query := "UPDATE admin SET password = ? WHERE username = ?"
 
 	_, err := tx.ExecContext(ctx, query, admin.Password, admin.Username)
 	if err != nil {
